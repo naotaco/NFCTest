e032418839a3e32f5520a50077fcd897e47e984d
Nao Taco <naotaco@gmail.com>
2014-02-11 02:20:15 +0900

    Initial commit for parsing Sony camera's NFC record

---
+++ NFCTest/MainPage.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using NFCTest.Resources;
+using NdefLibrary.Ndef;
+using Windows.Networking.Proximity;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace NFCTest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        private ProximityDevice _device;
+        private long _subscriptionIdNdef;
+        private long _publishingMessageId;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+
+
+            Loaded += (s, e) => {
+                this.initNFC();
+            };
+        }
+
+        private void initNFC()
+        {
+            // Initialize NFC
+            _device = ProximityDevice.GetDefault();
+            // Only subscribe for messages if no NDEF subscription is already active
+            if (_subscriptionIdNdef != 0) return;
+            // Ask the proximity device to inform us about any kind of NDEF message received from
+            // another device or tag.
+            // Store the subscription ID so that we can cancel it later.
+            _subscriptionIdNdef = _device.SubscribeForMessage("NDEF", MessageReceivedHandler);
+        }
+
+        private void MessageReceivedHandler(ProximityDevice sender, ProximityMessage message)
+        {
+            // Get the raw NDEF message data as byte array
+            var rawMsg = message.Data.ToArray();
+
+            var parser = new SonyNdefParser(rawMsg);
+
+            Debug.WriteLine("raw length: " + rawMsg.Length);
+
+            StringBuilder sb = new StringBuilder();
+            var values = new List<string>();
+
+            for (int i = 0; i < rawMsg.Length; i++)
+            {
+
+                int hex = (int)rawMsg[i];
+                char buf = (char)rawMsg[i];
+                // Debug.WriteLine(hex + " " + buf);
+
+                if (hex < 20)
+                {
+                    if (sb.ToString().Length > 2)
+                    {
+                        values.Add(sb.ToString());
+                    }
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.Append((char)rawMsg[i]);
+                }
+            }
+            values.Add(sb.ToString());
+
+            foreach (String str in values)
+            {
+                // Debug.WriteLine("append raw message: " + str);
+            }
+
+
+            // Let the NDEF library parse the NDEF message out of the raw byte array
+            var ndefMessage = NdefMessage.FromByteArray(rawMsg);
+
+            // Analysis result
+            var tagContents = new StringBuilder();
+
+            // Loop over all records contained in the NDEF message
+            foreach (NdefRecord record in ndefMessage)
+            {
+                // --------------------------------------------------------------------------
+                // Print generic information about the record
+                if (record.Id != null && record.Id.Length > 0)
+                {
+                    // Record ID (if present)
+                    tagContents.AppendFormat("Id: {0}\n", Encoding.UTF8.GetString(record.Id, 0, record.Id.Length));
+                }
+                
+                // Record type
+                if (record.Type != null && record.Type.Length > 0)
+                {
+                    tagContents.AppendFormat("Record type: {0}\n",
+                                             Encoding.UTF8.GetString(record.Type, 0, record.Type.Length));
+                }
+
+                if (record.Payload != null && record.Payload.Length > 0)
+                {
+                    tagContents.AppendFormat("Payload: {0}\n", Encoding.UTF8.GetString(record.Payload, 0, record.Payload.Length));
+                }
+
+                // --------------------------------------------------------------------------
+                // Check the type of each record
+                // Using 'true' as parameter for CheckSpecializedType() also checks for sub-types of records,
+                // e.g., it will return the SMS record type if a URI record starts with "sms:"
+                // If using 'false', a URI record will always be returned as Uri record and its contents won't be further analyzed
+                // Currently recognized sub-types are: SMS, Mailto, Tel, Nokia Accessories, NearSpeak, WpSettings
+                var specializedType = record.CheckSpecializedType(true);
+
+                if (specializedType == typeof(NdefMailtoRecord))
+                {
+                    // --------------------------------------------------------------------------
+                    // Convert and extract Mailto record info
+                    var mailtoRecord = new NdefMailtoRecord(record);
+                    tagContents.Append("-> Mailto record\n");
+                    tagContents.AppendFormat("Address: {0}\n", mailtoRecord.Address);
+                    tagContents.AppendFormat("Subject: {0}\n", mailtoRecord.Subject);
+                    tagContents.AppendFormat("Body: {0}\n", mailtoRecord.Body);
+                }
+                else if (specializedType == typeof(NdefUriRecord))
+                {
+                    // --------------------------------------------------------------------------
+                    // Convert and extract URI record info
+                    var uriRecord = new NdefUriRecord(record);
+                    tagContents.Append("-> URI record\n");
+                    tagContents.AppendFormat("URI: {0}\n", uriRecord.Uri);
+                }
+                else if (specializedType == typeof(NdefSpRecord))
+                {
+                    // --------------------------------------------------------------------------
+                    // Convert and extract Smart Poster info
+                    var spRecord = new NdefSpRecord(record);
+                    tagContents.Append("-> Smart Poster record\n");
+                    tagContents.AppendFormat("URI: {0}", spRecord.Uri);
+                    tagContents.AppendFormat("Titles: {0}", spRecord.TitleCount());
+                    if (spRecord.TitleCount() > 1)
+                        tagContents.AppendFormat("1. Title: {0}", spRecord.Titles[0].Text);
+                    tagContents.AppendFormat("Action set: {0}", spRecord.ActionInUse());
+                    // You can also check the action (if in use by the record), 
+                    // mime type and size of the linked content.
+                }
+                else if (specializedType == typeof(NdefLaunchAppRecord))
+                {
+                    // --------------------------------------------------------------------------
+                    // Convert and extract LaunchApp record info
+                    var launchAppRecord = new NdefLaunchAppRecord(record);
+                    tagContents.Append("-> LaunchApp record" + Environment.NewLine);
+                    if (!string.IsNullOrEmpty(launchAppRecord.Arguments))
+                        tagContents.AppendFormat("Arguments: {0}\n", launchAppRecord.Arguments);
+                    if (launchAppRecord.PlatformIds != null)
+                    {
+                        foreach (var platformIdTuple in launchAppRecord.PlatformIds)
+                        {
+                            if (platformIdTuple.Key != null)
+                                tagContents.AppendFormat("Platform: {0}\n", platformIdTuple.Key);
+                            if (platformIdTuple.Value != null)
+                                tagContents.AppendFormat("App ID: {0}\n", platformIdTuple.Value);
+                        }
+                    }
+                }
+                else
+                {
+                    if (record.Id != null)
+                    {
+                        String id = System.Text.Encoding.UTF8.GetString(record.Id, 0, record.Id.Length);
+                        tagContents.Append("ID: " + id + Environment.NewLine);
+                    }
+                    if (record.Payload != null)
+                    {
+                        String pl = System.Text.Encoding.UTF8.GetString(record.Payload, 0, record.Payload.Length);
+                        tagContents.Append("payload: " + pl + Environment.NewLine);
+                    }
+                    // Other type, not handled by this demo
+                    tagContents.Append("NDEF record not parsed by this demo app" + Environment.NewLine);
+                }
+            }
+        }
+
+    }
+}
---
+++ NFCTest/SonyNdefParser.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFCTest
+{
+    class SonyNdefParser
+    {
+
+        private Byte[] raw;
+        private List<SonyNdefRecord> records;
+
+        public SonyNdefParser(Byte[] input)
+        {
+            raw = input;
+            records = new List<SonyNdefRecord>();
+
+            this.parse();
+        }
+
+        private void parse()
+        {
+            int recordPointer = 0;
+
+            var record = new SonyNdefRecord();
+            record.ndefHeader = raw[recordPointer];
+
+            recordPointer++;
+
+            record.typeLength = raw[recordPointer];
+
+            // to check whether short record or not
+            if ((0x10 & record.ndefHeader) == 0x10)
+            {
+                // short length
+                recordPointer += 1;
+                record.payloadLength = raw[recordPointer];
+            }
+            else
+            {
+                // not sony nfc format
+                recordPointer += 4;
+            }
+
+            // to check id length (0 or 1)
+            if ((0x08 & record.ndefHeader) == 0x08)
+            {
+                record.idLength = 1;
+                recordPointer++;
+                record.idLength = (int)raw[recordPointer];
+            }
+            else
+            {
+                record.idLength = 0;
+                
+            }
+            
+            recordPointer++;
+
+            // get type
+            int typeEndPointer = recordPointer + record.typeLength;
+            StringBuilder sb = new StringBuilder();
+            for (; recordPointer < typeEndPointer; recordPointer++)
+            {
+                sb.Append((char)raw[recordPointer]);
+            }
+            record.type = sb.ToString();
+            sb.Clear();
+
+            recordPointer++;
+
+            // get id (if exist)
+            if (record.idLength > 0)
+            {
+                int idEndPointer = recordPointer + record.idLength;
+                for (; recordPointer < idEndPointer; recordPointer++)
+                {
+                    sb.Append((char)raw[recordPointer]);
+                }
+                record.id = sb.ToString();
+                sb.Clear();
+                recordPointer++;
+            }
+
+            // get payload
+            int payloadEndPointer = recordPointer + record.payloadLength;
+
+            Byte[] payload = new Byte[record.payloadLength];
+            Array.Copy(raw, recordPointer, payload, 0, record.payloadLength);
+
+            var parsedPayload = this.ParsePayload(payload);
+            record.SSID = parsedPayload.SSID;
+            record.Password = parsedPayload.Password;
+
+            for (; recordPointer < payloadEndPointer; recordPointer++)
+            {
+                char ch = (char)raw[recordPointer];
+                if ((int)ch > 0x19)
+                {
+                    // Debug.WriteLine(ch);
+                    sb.Append(ch);
+                }
+                else
+                {
+                    // Debug.WriteLine((int)ch);
+                }
+            }
+            record.payload = sb.ToString();
+            sb.Clear();
+
+            
+
+
+
+
+            record.dump();
+        }
+
+        private SonyNdefRecord ParsePayload(byte[] payload)
+        {
+            var ret = new SonyNdefRecord();
+            StringBuilder sb = new StringBuilder();
+
+            int SSIDStartPointer = 0;
+            int SSIDLength = 0;
+            int PasswordStartPointer = 0;
+            int PasswordLength = 0;
+
+            // find SSID
+            for (int i = 0; i < payload.Length; i++)
+            {
+                int val = (int)payload[i];
+                Debug.WriteLine((char)val);
+
+                if (val > 0x19 && i > 0)
+                {
+                    // ASCII
+                    SSIDStartPointer = i;
+                    SSIDLength = (int)payload[i - 1];
+                    break;
+                }
+            }
+
+            Debug.WriteLine("ssid starts from: " + SSIDStartPointer);
+            Debug.WriteLine("ssid length: " + SSIDLength);
+
+            // get ssid
+            for (int i = 0; i < SSIDLength; i++)
+            {
+                sb.Append((char)payload[SSIDStartPointer + i]);
+            }
+            ret.SSID = sb.ToString();
+            Debug.WriteLine("ssid: " + ret.SSID);
+
+            sb.Clear();
+
+            // find password
+            for (int i = SSIDStartPointer + SSIDLength; i < payload.Length; i++)
+            {
+
+                int val = (int)payload[i];
+
+                if (val > 0x19 && i > 0)
+                {
+                    // if ASCII found
+                    PasswordStartPointer = i;
+                    PasswordLength = (int)payload[i - 1];
+                    break;
+                }
+            }
+
+            Debug.WriteLine("password starts from: " + PasswordStartPointer);
+            Debug.WriteLine("password length: " + PasswordLength);
+
+            // get password
+            for (int i = 0; i < PasswordLength; i++)
+            {
+                sb.Append((char)payload[PasswordStartPointer + i]);
+            }
+            ret.Password = sb.ToString();
+
+
+
+            return ret;
+
+        }
+
+        
+
+    }
+}
